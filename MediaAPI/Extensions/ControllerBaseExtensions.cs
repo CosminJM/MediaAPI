17e36abf0e8133201854a2193c0346ac79763c17
Jacob Cosmin <cosmyn977@gmail.com>
2024-05-30 21:49:29 +0300

    Added ControllerBaseExtensions. Added Pagination class.

---
+++ MediaAPI/Extensions/ControllerBaseExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace MediaAPI.Extensions
+{
+    public static class ControllerExtensions
+    {
+        public static string GetUsernameFromToken(this ControllerBase controller)
+        {
+            return controller.User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+        }
+    }
+}
