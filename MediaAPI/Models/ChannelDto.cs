364d164006ef6bf34fdfbb751db23cec77491677
Jacob Cosmin <cosmyn977@gmail.com>
2024-06-07 23:25:23 +0300

    Updated types from int to Guid for mapper profiles.

--- MediaAPI/Models/ChannelDto.cs
+++ MediaAPI/Models/ChannelDto.cs
@@ -2,7 +2,7 @@
 {
     public class ChannelDto
     {
-        public int ChannelId { get; set; }
+        public Guid ChannelId { get; set; }
         public string? ChannelIdentificator { get; set; }
         public string? Name { get; set; }
     }
